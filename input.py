cada2afb064b9a62a05cc4b95099929fed78f296
talentceffort <talentceffort@gmail.com>
2019-08-17 14:49:52 +0900

    secondTest

---
+++ input.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*- 
+input('insert your name : ')
--- randomzie.py
+++ randomzie.py
@@ -1,4 +1,8 @@
+# -*- coding: utf-8 -*- 
 from random import randint, uniform
 
 #두 수 사이 임의의 정수 값 return
 print(randint(1, 10))
+
+#두 수 사이 임의의 실수 값 return
+print(uniform(1, 0))
